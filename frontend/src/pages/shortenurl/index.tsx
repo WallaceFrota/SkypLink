d1c4d6b35aa2dda4576eb1d4858bb0ac130d4e17
Wallace Frota <frotadev@gmail.com>
2020-12-03 22:23:30 -0300

    Arquivo principal que lida com o encurtamento do link

--- frontend/src/pages/shortenurl/index.tsx
+++ frontend/src/pages/shortenurl/index.tsx
@@ -1,5 +1,9 @@
-import React, { FormEvent, useState } from 'react';
-import {FiLink, FiCopy, FiX} from 'react-icons/fi'
+// DEVELOPED BY - WALLACE FROTA
+// FROTADEV - SOFTWARE HOUSE
+
+import React, { FormEvent, useRef, useState } from 'react';
+import {FiLink, FiCopy, FiX, FiClipboard} from 'react-icons/fi'
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -8,40 +12,58 @@ import '../../styles/pages/shortenurl.css';
 import logoShorten from '../../images/skyplink.png'
 import api from '../../services/api';
 
-export default function ShortenUrl() {
+function ShortenUrl() {
+    const urlCopy = useRef<HTMLInputElement>(null);
     const [visible, setVisible] = useState(false);
     const [url, setUrl] = useState('');
     const [shortenedUrl, setShortenedUrl] = useState('');
+    const [copied, setCopied] = useState(false);
 
+    // função para encurtar link
     async function handleShortenUrl(event: FormEvent) {
         event.preventDefault();
 
-        await api.post('/links', {
-            url: url
-        }).then(response => {
-            const {code} = response.data;
-            setShortenedUrl(code);
-            setUrl('')
-            setVisible(true)
-        })
+        if(!url) {
+            return alert("Preencha o campo!");
+        }
+        try {
+            await api.post('/links', {
+                url: url
+            }).then(response => {
+                const {code} = response.data;
+                setShortenedUrl(code);
+                setUrl('')
+                setVisible(true)
+            })
+        } catch (error) {
+            return alert("Erro ao encurtar url")
+        }
+    }
+    // função que copia link encurtado
+    const handleCopyToClipboard = () => {
+        if(urlCopy && urlCopy.current) {
+            urlCopy.current.select();
+            document.execCommand("copy");
+            setCopied(true)
+        }
     }
 
     // close modal
     function handleCloseModal() {
         setVisible(!visible)
+        setCopied(false)
     }
 
     return (
-        <div className="container">
+        <div className="page-container">
             <Header />
-                    <div id="shorten">
+                    <div id="shorten-form">
                             <form onSubmit={handleShortenUrl}>
                                 <div className="image">
                                     <img src={logoShorten} alt="Shorten"/>
                                 </div>
                                 
                                 <input
-                                    name="url"
                                     placeholder="Your url here"
                                     value={url}
                                     onChange={event => setUrl(event.target.value)}
@@ -53,26 +75,46 @@ export default function ShortenUrl() {
                                 </button>
                             </form>
                     </div>
-
                     {visible && (
-                        <div className="modal" >
+                        <div className="modal">
                             <div className="close-modal">
-                                <a href="#" onClick={handleCloseModal}>
-                                    <FiX color="#000" size={32}/>
-                                </a>
+                                <p>Acompanhe as estatísticas do link em https://skyplink.co/{shortenedUrl}/stats</p>
+                                <button type="button" onClick={handleCloseModal}>
+                                    <FiX color="#fff" size={26}/>
+                                </button>
                             </div>
+                                <input
+                                    defaultValue={`http://localhost:3000/${shortenedUrl}`}
+                                    ref={urlCopy}
+                                />
 
-                            <div className="modal-content">
-                                <input id="url" disabled value={`http://localhost:3333/links/${shortenedUrl}`}/>
-
-                                <a href="#" >
-                                    <span>Copiar</span> 
-                                    <FiCopy color="#fff" size={26} />
-                                </a>
-                            </div>
+                                <button type="button" onClick={handleCopyToClipboard}
+                                    className={copied ? "copied" : "btn-copy"}
+                                >
+                                    {copied ?
+                                        <>
+                                        <span>Copiado!</span>
+                                        </>
+                                        :
+                                        <>
+                                        <span>Copiar</span>
+                                        </>
+                                    }
+                                    {copied ?
+                                        <>
+                                            <FiClipboard color="#fff" size={26} />
+                                        </>
+                                        :
+                                        <>
+                                            <FiCopy color="#fff" size={26} />
+                                        </>
+                                    }
+                                </button>
                         </div>
                     )}
-            <Footer />
+                <Footer />
         </div>
     )
 }
+
+export default ShortenUrl;
