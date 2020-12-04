61300d080907f0ce794a7cc501b98d6300818420
Wallace Frota <frotadev@gmail.com>
2020-12-03 22:18:46 -0300

    Adicionado novas rotas

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 import {BrowserRouter, Switch, Route, } from 'react-router-dom';
 
 import ShortenUrl from './pages/ShortenUrl';
-import UrlStats from './pages/UrlStats';
 import NotFound from './pages/NotFound';
+import RedirectPage from './pages/RedirectPage';
+import UrlStats from './pages/UrlStats';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={ShortenUrl}/>
-                <Route path="/:code/stats" component={UrlStats}/>
-                <Route path="/*" component={NotFound}/>
+                <Route exact path="/:link" component={RedirectPage}/>
+                <Route exact path="/:link/stats" component={UrlStats}/>
+                <Route  path="/*" component={NotFound}/>
             </Switch>
         </BrowserRouter>
     )
