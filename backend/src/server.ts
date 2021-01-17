53945be2d18a86b712585bc5ba40a09bf636bc8e
Wallace Frota <frotadev@gmail.com>
2021-01-16 22:37:55 -0300

    updated archives

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
 // importando rotas
 import routes from './routes';
 
+app.use(cors());
 app.use(express.json());
 
 // utilizando rotas
